132b07c5261498ec494813c5b5147086e5be8335
Parviz <parviz.hajili@gmail.com>
2023-03-24 14:26:52 +0400

    applied orm

---
+++ Riode.WebUI/Models/DataContexts/RiodeDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Riode.WebUI.Models.Entities;
+
+namespace Riode.WebUI.Models.DataContexts
+{
+    public class RiodeDbContext : DbContext
+    {
+        public RiodeDbContext() : base()
+        {
+
+        }
+
+        public RiodeDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Data Source=Localhost;Initial Catalog=RiodeDb;Integrated Security=true;");
+            }
+        }
+
+        public DbSet<Contact> Contacts { get; set; }
+    }
+}
