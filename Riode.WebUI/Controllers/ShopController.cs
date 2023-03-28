b13acb823a04e654939b0010588cd56bc75eda6b
Parviz <parviz.hajili@gmail.com>
2023-03-29 02:05:11 +0400

    add ShopFilterViewModel

--- Riode.WebUI/Controllers/ShopController.cs
+++ Riode.WebUI/Controllers/ShopController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Riode.WebUI.Models.DataContexts;
 using Riode.WebUI.Models.Entities;
+using Riode.WebUI.Models.ViewModels;
 
 namespace Riode.WebUI.Controllers
 {
@@ -9,11 +11,26 @@ namespace Riode.WebUI.Controllers
         public IActionResult Index()
         {
             RiodeDbContext dbContext = new RiodeDbContext();
-            List<Brand> brands = dbContext.Brands
+            ShopFilterViewModel viewModel = new();
+
+            viewModel.Brands = dbContext.Brands
                 .Where(b => b.DeletedByUserId == null)
                 .ToList();
 
-            return View(brands);
+            viewModel.ProductColors = dbContext.ProductColors
+                .Where(x => x.DeletedByUserId == null)
+                .ToList();
+
+            viewModel.ProductSizes = dbContext.ProductSizes
+                .Where(x => x.DeletedByUserId == null)
+                .ToList();
+
+            viewModel.Categories = dbContext.Categories
+                .Include(x => x.Children)
+                .Where(x => x.DeletedByUserId == null && x.ParentId == null)
+                .ToList();
+
+            return View(viewModel);
         }
 
         public IActionResult Details()
