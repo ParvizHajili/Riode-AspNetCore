bb9fcd4c5da02d7c0a58aef461aca5cc01e07c15
Parviz <parviz.hajili@gmail.com>
2023-03-28 23:48:56 +0400

    some changes for brands

--- Riode.WebUI/Models/Entities/Brand.cs
+++ Riode.WebUI/Models/Entities/Brand.cs
@@ -4,7 +4,7 @@
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public string Description { get; set; }
+        public string? Description { get; set; }
         public int? CreateByUserId { get; set; }
         public DateTime CreatedDate { get; set; }
         public int? DeletedByUserId { get; set; }
